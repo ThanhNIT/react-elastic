6d5a58ef8c4420ea93b1a8cd01273ebaba01dabd
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2024-02-20 13:44:45 +0700

    update process pdf

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -7,10 +7,8 @@ const SearchResults = ({ results, handleSelect }) => {
     <div className="search-results-container"> {/* Add a container div with a class */}
       {results.map((result, index) => (
         <div onClick={e=> handleSelect(result.title)} key={index} className="search-result-item"> {/* Add a class for each result */}
-          <h2>{result.title}</h2>
+          <h2>{result.docUrl}</h2>
           <p>{result.content}</p>
-          <p>Author: {result.author}</p>
-          <p>Date: {result.date}</p>
           {/* Add any other fields from _source that you want to display */}
         </div>
       ))}
